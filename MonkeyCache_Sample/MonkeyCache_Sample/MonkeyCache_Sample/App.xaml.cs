b03b95a0e21977953907d3162f290aeaef804e4e
宇薪 邱 <simoncat524@hotmail.com>
2020-02-08 12:57:21 +0800

    Add JOSN_Server

---
+++ MonkeyCache_Sample/MonkeyCache_Sample/MonkeyCache_Sample/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MonkeyCache_Sample.Services;
+using MonkeyCache_Sample.Views;
+
+namespace MonkeyCache_Sample
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MonkeyCache_Sample/MonkeyCache_Sample/MonkeyCache_Sample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MonkeyCache_Sample.Models;
+
+namespace MonkeyCache_Sample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
